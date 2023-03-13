0e9e3821cdc27d304d8511f6b8c0c57833aa17cd
Harry <harry.n.griffiths@gmail.com>
2023-03-13 15:52:45 +0000

    Started on a very small C compiler c_small
    Co-authored-by: Samsam Lee <jieunboy0516@users.noreply.github.com>

---
+++ src/c_small/src/c_small_main.cpp
@@ -0,0 +1,40 @@
+//#include "lexertest.hpp"
+
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <iomanip>
+
+#include "./include/ast.hpp"
+#include "../bin/c_small.tab.hpp"
+
+
+
+int main()
+{
+
+    while(1){
+        TokenType type=(TokenType)yylex();
+
+        //std::string word = *yylval.string;
+
+        std::cout << type << std::endl;
+        //delete yylval.string;
+
+        // Parse the AST
+        
+        
+        // evaluate it with the bindings given
+        
+        std::cout<<std::endl;
+
+      
+    }
+
+    // const Expression *ast=parseAST();
+    // ast->print(std::cout);
+
+    return 0;
+}
